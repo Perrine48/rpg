f876c577b0b9b9654a28928d8629840f22f82f06
Perrine <perrine.salce@hotmail.com>
2018-08-24 10:57:05 +0200

    Test 2 ok

--- core/board.py
+++ core/board.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 # -- coding: UTF-8 --
-
 class Board():
     def __init__(self, nb_row, nb_col):
         self.grid = []
-        for lig in range(nb_row):
+        for ligne in range(nb_row):
             self.grid.append([])
-            self.grid[lig].append([] * nb_col)
+            for col in range(nb_col):
+                self.grid[ligne].append([])
 
     def move(self, character, x, y):
+        # if any(character in ligne for ligne in self.grid):
+        # for ligne in range(len(self.grid)):
+        if character.x != None and character.y != None:
+            self.grid[character.x][character.y].remove(character)
+
         character.x = x
         character.y = y
         self.grid[x][y] = [character]
